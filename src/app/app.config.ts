d363aa0021eae83d96d60ffbad9cfe8d257c218d
Connie Leung <cleung334@gmail.com>
2026-06-26 20:12:59 +0800

    chore: add number to spec to show order of work

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,12 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZonelessChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { provideRouter, withComponentInputBinding, withExperimentalAutoCleanupInjectors } from '@angular/router';
 
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideBrowserGlobalErrorListeners(), provideZonelessChangeDetection(), provideRouter(routes)],
+  providers: [
+    provideBrowserGlobalErrorListeners(),
+    provideZonelessChangeDetection(),
+    provideRouter(routes, withComponentInputBinding(), withExperimentalAutoCleanupInjectors()),
+  ],
 };
